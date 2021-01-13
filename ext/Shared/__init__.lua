7ff863bf40c73bdd714939c4a2ec72a0198c78cd
Joe91 <1015.5@gmx.de>
2021-01-13 20:12:59 +0100

    initial commit

--- ext/Shared/__init__.lua
+++ ext/Shared/__init__.lua
@@ -1,13 +1,1 @@
-
-Events:Subscribe('Partition:Loaded', function(partition)
-	for _, instance in pairs(partition.instances) do
-		if instance:Is('GunSwayData') then
-			instance = GunSwayData(instance)
-			-- Make it writable so we can modify its fields.
-			instance:MakeWritable()
-
-			instance.deviationScaleFactorNoZoom = 0.9
-			instance.gameplayDeviationScaleFactorNoZoom = 0.9
-		end
-	end
-end)
+Events:Subscribe('Partition:Loaded', function(partition)	for _, instance in pairs(partition.instances) do		if instance:Is('GunSwayData') then			instance = GunSwayData(instance)			-- Make it writable so we can modify its fields.			instance:MakeWritable()			instance.deviationScaleFactorNoZoom = 0.7			instance.gameplayDeviationScaleFactorNoZoom = 0.7		end	endend)
