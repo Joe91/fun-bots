47ef45744b3d0ec6f6d19723424e103501bab308
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-01-18 19:07:13 +0100

    Beautify

--- ext/Client/__init__.lua
+++ ext/Client/__init__.lua
@@ -1,23 +1,24 @@
-class('FunBotClient')
-local FunBotUIClient = require('UIClient')
-local ClientBotManager = require('clientBotManager')
+class('FunBotClient');
+
+local FunBotUIClient	= require('UIClient');
+local ClientBotManager	= require('ClientBotManager');
 
 function FunBotClient:__init()
-	Events:Subscribe('Extension:Unloading', self, self._onExtensionUnload)
-	Events:Subscribe('Engine:Message', self, self._onEngineMessage)
+	Events:Subscribe('Extension:Unloading', self, self._onExtensionUnload);
+	Events:Subscribe('Engine:Message', self, self._onEngineMessage);
 end
 
 function FunBotClient:_onExtensionUnload()
-	ClientBotManager:onExtensionUnload()
+	ClientBotManager:onExtensionUnload();
 end
 
 function FunBotClient:_onEngineMessage(p_Message)
-	ClientBotManager:onEngineMessage(p_Message)
+	ClientBotManager:onEngineMessage(p_Message);
 end
 
 -- Singleton.
-if g_FunBotClient == nil then
-	g_FunBotClient = FunBotClient()
+if (g_FunBotClient == nil) then
+	g_FunBotClient = FunBotClient();
 end
 
-return g_FunBotClient
+return g_FunBotClient;
