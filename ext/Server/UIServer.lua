2817ef115b60e7538a5181593eb6107ecda90298
Joe91 <1015.5@gmx.de>
2021-01-20 20:13:56 +0100

    save temp work

--- ext/Server/UIServer.lua
+++ ext/Server/UIServer.lua
@@ -167,13 +167,81 @@ function FunBotUIServer:_writeSettings(player, request)
 		temporary = (request.subaction == 'temp');
 	end
 	
+	if request.botWeapon ~= nil then
+		local tempString = request.botWeapon;
+
+		for _, weapon in pairs(BotWeapons) do
+			if tempString == weapon then
+				SettingsManager:update('botWeapon', tempString, temporary);
+				break
+			end
+		end
+	end
+
+	if request.botKit ~= nil then
+		local tempString = request.botKit;
+
+		for _, kit in pairs(BotKits) do
+			if tempString == kit then
+				SettingsManager:update('botKit', tempString, temporary);
+				break
+			end
+		end
+	end
+
+	if request.botColor ~= nil then
+		local tempString = request.botColor;
+
+		for _, color in pairs(BotColors) do
+			if tempString == color then
+				SettingsManager:update('botColor', tempString, temporary);
+				break
+			end
+		end
+	end
+
+
+	settings:addList("GLOBAL", "botWeapon", "Bot Weapon", BotWeapons, data.botWeapon, "Primary", "Select the weapon the bots use");
+	settings:addList("GLOBAL", "botKit", "Bot Kit", BotKits, data.botKit, "RANDOM_KIT", "The Kit a bots spawns with.");
+	settings:addList("GLOBAL", "botColor", "Bot Color", BotColors, data.botColor, "RANDOM_COLOR", "The Kit-Color a bots spawns with.");
+
+	settings:add("DIFFICULTY", "Number", "botAimWorsening", "Aim Worsening", data.botAimWorsening * 100, 0, "0 = hard, 100 (or higher) = easy. Only takes effect on level Start");
+	settings:add("DIFFICULTY", "Number", "bulletDamageBot", "Damage Bot Bullet", data.bulletDamageBot, 9, "The damage a normal Bullet does");
+	settings:add("DIFFICULTY", "Number", "bulletDamageBotSniper", "Damage Bot Sniper", data.bulletDamageBotSniper, 24, "The damage a Sniper-Bullet does");
+	settings:add("DIFFICULTY", "Number", "meleeDamageBot", "Damage Bot Melee", data.meleeDamageBot, 42, "The Damage a melee-attack does");
+
+
+	settings:add("ADVANCED", "Number", "fovForShooting", "Bot FOV", data.fovForShooting, 270, "The Field Of View of the bots, where they can detect a player");
+	settings:add("ADVANCED", "Boolean", "shootBackIfHit", "Attack if Hit", data.shootBackIfHit, true, "Bots imidiatly attack player, if shot by it");
+	settings:add("ADVANCED", "Number", "maxRaycastDistance", "View Distance Bots", data.maxRaycastDistance, 125, "The maximum distance a bots starts shooting at a player");
+	settings:add("ADVANCED", "Number", "distanceForDirectAttack", "Direct Attack Distance", data.distanceForDirectAttack, 3, "When this close to a bot, he starts attacking");
+	settings:add("ADVANCED", "Boolean", "meleeAttackIfClose", "Attack with Melee", data.meleeAttackIfClose, true, "Bots attack the playe with the knife, if close");
+	settings:add("ADVANCED", "Boolean", "attackWayBots", "Attack other players", data.attackWayBots, true, "Bots on paths attack player by default");
+	settings:add("ADVANCED", "Number", "meleeAttackCoolDown", "Melee Cooldown", data.meleeAttackCoolDown, 3, "the time a Bot waits before attacking with melee again");
+	settings:add("ADVANCED", "Boolean", "jumpWhileShooting", "Allow Jump while shooting", data.jumpWhileShooting, true, "Bots jump over obstacles while shooting");
+
+	settings:add("ADVANCED", "Boolean", "spawnOnLevelstart", "Spawn on Levelstart", data.spawnOnLevelstart, true, "Bots spawn on levelstart (if valid paths are available)");
+	settings:add("ADVANCED", "Number", "initNumberOfBots", "Number Bots on Levelstart", data.initNumberOfBots, 10, "Bots on levelstart");
+	settings:add("ADVANCED", "Number", "spawnDelayBots", "Respawn Delay", data.spawnDelayBots, 2, "time till bots respawn, if respawn enabled");
+	settings:add("ADVANCED", "Number", "botTeam", "Default Bot Team", data.botTeam, 2, "default bot team 1 = US, 2 = RU");
+	settings:add("ADVANCED", "Boolean", "respawnWayBots", "Respawn Bots", data.respawnWayBots, true, "Bots on paths respawn if killed on startup");
+	settings:add("ADVANCED", "Boolean", "botNewLoadoutOnSpawn", "New Loadout on Spawn", data.botNewLoadoutOnSpawn, true, "bots get a new kit and color, if they respawn");
+	settings:add("ADVANCED", "Number", "maxAssaultBots", "Max Assault Bots", data.maxAssaultBots, -1, "maximum number of Bots with Assault Kit. -1 = unlimited");
+	settings:add("ADVANCED", "Number", "maxEngineerBots", "Max Engineer Bots", data.maxEngineerBots, -1, "maximum number of Bots with Engineer Kit. -1 = unlimited");
+	settings:add("ADVANCED", "Number", "maxSupportBots", "Max Support Bots", data.maxSupportBots, -1, "maximum number of Bots with Support Kit. -1 = unlimited");
+	settings:add("ADVANCED", "Number", "maxReconBots", "Max Recon Bots", data.maxReconBots, -1, "maximum number of Bots with Recon Kit. -1 = unlimited");
+
+
+	settings:add("OTHER", "Boolean", "disableChatCommands", "Disable Chat Commands", data.disableChatCommands, true, "if true, no chat commands can be used");
+	settings:add("OTHER", "Boolean", "traceUsageAllowed", "Allow Trace Usage", data.traceUsageAllowed, true, "if false, no traces can be recorded, deleted or saved");
+	settings:addList("OTHER", "language", "Language", { "de_DE", "cn_CN", "en_US" }, data.language, "en_US", "Select the language of this mod");
+	settings:add("OTHER", "Password", "settingsPassword", "Password", data.settingsPassword, nil, "Password protection of these Mod");
+
+
+
 	--global settings
 	if request.spawnInSameTeam ~= nil then
 		SettingsManager:update('spawnInSameTeam', (request.spawnInSameTeam == true), temporary);
-	end
-
-	if request.disableChatCommands ~= nil then
-		SettingsManager:update('disableChatCommands', (request.disableChatCommands == true), temporary);
 	end
 
 	if request.fovForShooting ~= nil then
@@ -228,38 +296,6 @@ function FunBotUIServer:_writeSettings(player, request)
 		end
 	end
 
-	if request.botWeapon ~= nil then
-		local tempString = request.botWeapon;
-
-		for _, weapon in pairs(BotWeapons) do
-			if tempString == weapon then
-				SettingsManager:update('botWeapon', tempString, temporary);
-				break
-			end
-		end
-	end
-
-	if request.botKit ~= nil then
-		local tempString = request.botKit;
-
-		for _, kit in pairs(BotKits) do
-			if tempString == kit then
-				SettingsManager:update('botKit', tempString, temporary);
-				break
-			end
-		end
-	end
-
-	if request.botColor ~= nil then
-		local tempString = request.botColor;
-
-		for _, color in pairs(BotColors) do
-			if tempString == color then
-				SettingsManager:update('botColor', tempString, temporary);
-				break
-			end
-		end
-	end
 
 	--client settings
 	if request.maxRaycastDistance ~= nil then
@@ -283,7 +319,7 @@ function FunBotUIServer:_writeSettings(player, request)
 		SettingsManager:update('language', request.value, temporary);
 	end
 	
-	-- Password
+	-- Other
 	if request.settingsPassword ~= nil then
 		if request.value == "" then
 			request.value = nil;
