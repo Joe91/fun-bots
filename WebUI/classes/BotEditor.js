59f0671cb944874e4d157ea0d29e179bbf848be1
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-01-21 19:20:00 +0100

    Fix previous-arrow, move constants & classes

--- WebUI/classes/BotEditor.js
+++ WebUI/classes/BotEditor.js
@@ -1,189 +1,5 @@
 let Language = {};
 
-const EntryElement = function EntryElement() {
-	let _element		= null;
-	let _type			= null;
-	let _name			= null;
-	let _title			= null;
-	let _value			= null;
-	let _default		= null;
-	let _list			= null;
-	let _list_index		= 0;
-	let _description	= null;
-	let _container		= null;
-
-	this.__constructor = function __constructor() {
-		_element	= document.createElement('ui-entry');
-		_container	= document.createElement('ui-container');
-
-		_element.onPrevious	= this.onPrevious.bind(this);
-		_element.onNext		= this.onNext.bind(this);
-	};
-
-	this.setType = function setType(type) {
-		_type					= type;
-		_element.dataset.type	= type;
-
-		let arrow_left			= this._createArrow('left');
-		let arrow_right			= this._createArrow('right');
-
-		switch(_type) {
-			case 'Boolean':
-				_container.appendChild(arrow_left);
-				_container.appendChild(this._createText(_value == null ? (_default == null ? '' : (_default ? 'Yes' : 'No')) : (_value ? 'Yes' : 'No')));
-				_container.appendChild(arrow_right);
-			break;
-			case 'Integer':
-			case 'Float':
-				_container.appendChild(arrow_left);
-				_container.appendChild(this._createInput('number', _value == null ? (_default == null ? '' : _default) : _value));
-				_container.appendChild(arrow_right);
-			break;
-			case 'List':
-				_container.appendChild(arrow_left);
-				_container.appendChild(this._createText(_value == null ? (_default == null ? '' : _default) : _value));
-				_container.appendChild(arrow_right);
-			break;
-			case 'Text':
-				_container.appendChild(this._createInput('text', _value == null ? (_default == null ? '' : _default) : _value));
-			break;
-			case 'Password':
-				_container.appendChild(this._createInput('password', _value == null ? (_default == null ? '' : _default) : _value));
-			break;
-		}
-	};
-
-	this._createText = function _createText(text) {
-		let element			= document.createElement('ui-text');
-		element.innerHTML	= text;
-		return element;
-	};
-
-	this._createInput = function _createInput(type, value) {
-		let element			= document.createElement('input');
-		element.type		= type;
-		element.value		= value;
-		return element;
-	};
-
-	this._createArrow = function _createArrow(direction) {
-		let arrow				= document.createElement('ui-arrow');
-		arrow.dataset.direction	= direction;
-		return arrow;
-	}
-
-	this.setName = function setName(name) {
-		_name					= name;
-		_element.dataset.name	= name;
-	};
-
-	this.setTitle = function setTitle(title) {
-		_title			= title;
-		let name		= document.createElement('ui-name');
-		name.innerHTML	= _title;
-		_element.appendChild(name);
-	};
-
-	this.onPrevious = function onPrevious() {
-		switch(_type) {
-			case 'Boolean':
-				this.setValue(!_value);
-			break;
-			case 'Integer':
-				this.setValue(_value - 1);
-			break;
-			case 'Float':
-				this.setValue(_value - 0.1);
-			break;
-			case 'List':
-				console.log(_list);
-				console.log('Old list index', _list_index);
-				--_list_index;
-
-				console.log('New list index', _list_index);
-				if(_list_index > 0) {
-					_list_index = _list.length - 1;
-				}
-				console.log('Updated list index', _list_index);
-
-
-				this.setValue(_list[_list_index]);
-			break;
-		}
-	};
-
-	this.onNext = function onNext() {
-		switch(_type) {
-			case 'Boolean':
-				this.setValue(!_value);
-			break;
-			case 'Integer':
-				this.setValue(_value + 1);
-			break;
-			case 'Float':
-				this.setValue(_value + 0.1);
-			break;
-			case 'List':
-				console.log(_list);
-				console.log('Old list index', _list_index);
-				++_list_index;
-				console.log('New list index', _list_index);
-
-				if(_list_index >= _list.length) {
-					_list_index = 0;
-				}
-				console.log('Updated list index', _list_index);
-				this.setValue(_list[_list_index]);
-			break;
-		}
-	};
-
-	this.setValue = function setValue(value) {
-		_value = value;
-
-		switch(_type) {
-			case 'Boolean':
-				_container.querySelector('ui-text').innerHTML = (_value ? 'Yes' : 'No');
-			break;
-			case 'Integer':
-				_value = parseInt(value, 10);
-				_container.querySelector('input[type="number"]').value = _value;
-			break;
-			case 'Float':
-				_value = parseFloat(value);
-				_container.querySelector('input[type="number"]').value = _value.toFixed(2);
-			break;
-			case 'List':
-				_container.querySelector('ui-text').innerHTML = _value;
-			break;
-		}
-	};
-
-	this.setDefault = function setDefault(value) {
-		_default					= value;
-		_element.dataset.default	= value;
-	};
-
-	this.setList = function setList(list) {
-		_list		= list;
-		_list_index	= 0;
-	};
-
-	this.setDescription = function setDescription(description) {
-		_description 					= description;
-		_element.dataset.description	= description;
-	};
-
-	this.getElement = function getElement() {
-		_element.appendChild(_container);
-		return _element;
-	};
-
-	this.__constructor.apply(this, arguments);
-};
-
-customElements.define('ui-entry', EntryElement, { extends: 'div' });
-
 const BotEditor = (new function BotEditor() {
 	const DEBUG				= false;
 	const VERSION			= '1.0.0-Beta';
@@ -209,8 +25,6 @@ const BotEditor = (new function BotEditor() {
 
 		this.bindMouseEvents();
 		this.bindKeyboardEvents();
-
-		//this.openSettings('[{"title":"Spawn in Same Team","category":"GLOBAL","name":"spawnInSameTeam","description":"If true, Bots spawn in the team of the player","types":"Boolean","value":"false","default":"false"},{"title":"Bot FOV","category":"GLOBAL","name":"fovForShooting","description":"The Field Of View of the bots, where they can detect a player","types":"Number","value":"270.0","default":"270"},{"title":"Damage Bot Bullet","category":"GLOBAL","name":"bulletDamageBot","description":"The damage a normal Bullet does","types":"Number","value":"10.0","default":"9"},{"title":"Damage Bot Sniper","category":"TRACE","name":"bulletDamageBotSniper","description":"The damage a Sniper-Bullet does","types":"Number","value":"24.0","default":"24"},{"title":"Damage Bot Melee","category":"TRACE","name":"meleeDamageBot","description":"The Damage a melee-attack does","types":"Number","value":"48.0","default":"42"},{"title":"Attack with Melee","category":"TRACE","name":"meleeAttackIfClose","description":"Bots attack the playe with the knife, if close","types":"Boolean","value":"true","default":"true"},{"title":"Attack if Hit","category":"OTHER","name":"shootBackIfHit","description":"Bots imidiatly attack player, if shot by it","types":"Boolean","value":"true","default":"true"},{"title":"Aim Worsening","category":"OTHER","name":"botAimWorsening","description":"0.0 = hard, 1.0 (or higher) = easy (and all between). Only takes effect on level Start","types":"Number","value":"0.0","default":"0.0"},{"description":"The Kit a bots spawns with. If Random is selected a random color is chosen. See config.lua for Kits","title":"Bot Kit","category":"OTHER","name":"botKit","default":"RANDOM_KIT","types":"List","value":"RANDOM_KIT"},{"description":"The Kit-Color a bots spawns with.  If Random is selected  a random color is chosen. See config.lua for colors","title":"Bot Color","category":"OTHER","name":"botColor","default":"RANDOM_COLOR","types":"List","value":"RANDOM_COLOR","list":["RANDOM_COLOR","Urban","ExpForce","Ninja","DrPepper","Para","Ranger","Specact","Veteran","Desert02","Green","Jungle","Navy","Wood01"]},{"description":"Select the language of this mod","title":"Language","category":"OTHER","name":"language","default":"en_US","types":"List","value":"en_US","list":["de_DE","en_US"]},{"title":"Password","category":"OTHER","name":"settingsPassword","description":"Password protection of these Mod","types":"Password","value":"nil"}]');
 	};
 
 	this.bindMouseEvents = function bindMouseEvents() {
@@ -560,14 +374,14 @@ const BotEditor = (new function BotEditor() {
 			output.setDescription(entry.description);
 
 			switch(entry.types) {
-				case 'List':
+				case EntryType.List:
 					output.setList(entry.list);
 				break;
-				case 'Boolean':
-				case 'Float':
-				case 'Integer':
-				case 'Text':
-				case 'Password':
+				case EntryType.Boolean:
+				case EntryType.Float:
+				case EntryType.Integer:
+				case EntryType.Text:
+				case EntryType.Password:
 
 				break;
 			}
---
+++ WebUI/classes/Constants.js
@@ -0,0 +1,8 @@
+const EntryType = {
+	Boolean:		'Boolean',
+	Integer:		'Integer',
+	Float:			'Float',
+	List:			'List',
+	Text:			'Text',
+	Password:		'Password'
+};
