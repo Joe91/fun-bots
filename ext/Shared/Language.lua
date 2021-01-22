3fe0ccccbc8ea6a62a1a1caa3284339b5016f22f
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-01-22 13:57:12 +0100

    Language skeleton

--- ext/Shared/Language.lua
+++ ext/Shared/Language.lua
@@ -1,13 +1,24 @@
 class 'Language';
 
 function Language:__init()
-	
+    self:loadLanguage(Config.language);
 end
 
-function ArrayMap:I18N(string)
+function Language:loadLanguage(name)
+    require('__shared/Languages/' .. name .. '.lua');
+end
+
+function Language:I18N(string)
 	-- @ToDo check if language-file exists and translate.
-  
-  return string;
+    if (Languages ~= nil ) then
+	if (Languages[Config.language] ~= nil ) then
+	    if (Languages[Config.language][string] ~= nil ) then
+	        return Languages[Config.language][string];
+	    end
+	end
+    end
+    
+    return string;
 end
 
 if (g_Language == nil) then
