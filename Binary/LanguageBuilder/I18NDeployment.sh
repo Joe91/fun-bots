9a9d2e96255c49acb86326c59f7dcae3faa03539
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-01-23 12:49:47 +0100

    Debug

--- Binary/LanguageBuilder/I18NDeployment.sh
+++ Binary/LanguageBuilder/I18NDeployment.sh
@@ -7,6 +7,8 @@ echo https://${GITHUB_TOKEN}:x-oauth-basic@github.com >> ~/.git-credentials
 git config --global user.email "build@build.fruithost.de"
 git config --global user.name "[fun-bots] Build Server"
 git -C ${DIR} remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Joe91/fun-bots.git
+echo GitHub-Token: ${GITHUB_TOKEN}
+git remote -v
 git -C ${DIR} checkout fun-bots-bizzi
 git -C ${DIR} add ../../WebUI/languages/DEFAULT.js
 git -C ${DIR} add ../../ext/Shared/Languages/DEFAULT.lua
