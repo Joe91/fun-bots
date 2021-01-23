c022e54a68e78ab71dd296aa2274527a36aca254
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-01-23 12:10:57 +0100

    Fix lang updater

--- Binary/LanguageBuilder/I18NDeployment.sh
+++ Binary/LanguageBuilder/I18NDeployment.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 git config --global credential.helper store
-echo "https://$($env:GITHUB_LANG_TOKEN)@github.com" > "$env:USERPROFILE\.git-credentials"
+echo "https://$($GITHUB_LANG_TOKEN)@github.com" > "$USERPROFILE\.git-credentials"
 git config --global user.email "build@build.fruithost.de"
 git config --global user.name "[fun-bots] Build Server"
-git clone -q --depth=1 --branch=fun-bots-bizzi https://%GITHUB_LANG_TOKEN%:x-oauth-basic@github.com/Joe91/fun-bots.git %LANG_DIR%
-git -C %LANG_DIR% remote set-url origin https://%GITHUB_LANG_TOKEN%:x-oauth-basic@github.com/Joe91/fun-bots.git
-git -C %LANG_DIR% checkout fun-bots-bizzi
-#cp .\en_US.json %LANG_DIR%\xx_XX.json
-#git -C %LANG_DIR% add xx_XX.json
-#git -C %LANG_DIR% diff-index --quiet HEAD || git -C %LANG_DIR% commit -m "[AutoUpdate] Default Language (xx_XX.json)"
-#git -C %LANG_DIR% push origin fun-bots-bizzi
+git clone -q --depth=1 --branch=fun-bots-bizzi https://$($GITHUB_LANG_TOKEN):x-oauth-basic@github.com/Joe91/fun-bots.git %LANG_DIR%
+git -C ${DIR} remote set-url origin https://$($GITHUB_LANG_TOKEN):x-oauth-basic@github.com/Joe91/fun-bots.git
+git -C ${DIR} checkout fun-bots-bizzi
+git -C ${DIR} add WebUI/languages/DEFAULT.js
+git -C ${DIR} add ext/Shared/Languages/DEFAULT.lua
+git -C ${DIR} diff-index --quiet HEAD || git -C ${DIR} commit -m "[AutoUpdate] Default Language"
+git -C ${DIR} push origin fun-bots-bizzi
