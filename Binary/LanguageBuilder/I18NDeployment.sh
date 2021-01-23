7fcb7e509e89efc4a46b0f4eeea14a11aa2daa03
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-01-23 13:01:03 +0100

    Fix auth

--- Binary/LanguageBuilder/I18NDeployment.sh
+++ Binary/LanguageBuilder/I18NDeployment.sh
@@ -2,15 +2,24 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+# Auth helper
 git config --global credential.helper store
 echo https://${GITHUB_TOKEN}:x-oauth-basic@github.com >> ~/.git-credentials
-git config --global user.email "build@build.fruithost.de"
+git config --global user.email "build@bizzi.appveyor.com"
 git config --global user.name "[fun-bots] Build Server"
-git -C ${DIR} remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Joe91/fun-bots.git
+#git -C ${DIR} remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Joe91/fun-bots.git
+
+# Debug
 echo GitHub-Token: ${GITHUB_TOKEN}
 git remote -v
+
+# Check out Branch
 git -C ${DIR} checkout fun-bots-bizzi
+
+# Add files
 git -C ${DIR} add ../../WebUI/languages/DEFAULT.js
 git -C ${DIR} add ../../ext/Shared/Languages/DEFAULT.lua
+
+# Check difference otherwise commit and push
 git -C ${DIR} diff-index --quiet HEAD || git -C ${DIR} commit -m "[AutoUpdate] Default Language"
 git -C ${DIR} push origin fun-bots-bizzi
