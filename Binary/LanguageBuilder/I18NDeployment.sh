d0307da70d65d6b7f704c74b879af1331af2eda6
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-01-23 12:19:15 +0100

    Fix path

--- Binary/LanguageBuilder/I18NDeployment.sh
+++ Binary/LanguageBuilder/I18NDeployment.sh
@@ -9,7 +9,7 @@ git config --global user.name "[fun-bots] Build Server"
 git clone -q --depth=1 --branch=fun-bots-bizzi https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Joe91/fun-bots.git %LANG_DIR%
 git -C ${DIR} remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Joe91/fun-bots.git
 git -C ${DIR} checkout fun-bots-bizzi
-git -C ${DIR} add WebUI/languages/DEFAULT.js
-git -C ${DIR} add ext/Shared/Languages/DEFAULT.lua
+git -C ${DIR} add ../../WebUI/languages/DEFAULT.js
+git -C ${DIR} add ../../ext/Shared/Languages/DEFAULT.lua
 git -C ${DIR} diff-index --quiet HEAD || git -C ${DIR} commit -m "[AutoUpdate] Default Language"
 git -C ${DIR} push origin fun-bots-bizzi
