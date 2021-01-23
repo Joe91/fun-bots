5e1c689fde353e5a9a3547c894d351461850712b
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-01-23 12:34:13 +0100

    Testing git

--- Binary/LanguageBuilder/I18NDeployment.sh
+++ Binary/LanguageBuilder/I18NDeployment.sh
@@ -6,8 +6,6 @@ git config --global credential.helper store
 echo "https://${GITHUB_TOKEN}@github.com" > "${USERPROFILE}\.git-credentials"
 git config --global user.email "build@build.fruithost.de"
 git config --global user.name "[fun-bots] Build Server"
-git clone -q --depth=1 --branch=fun-bots-bizzi https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Joe91/fun-bots.git ${DIR}
-git -C ${DIR} remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Joe91/fun-bots.git
 git -C ${DIR} checkout fun-bots-bizzi
 git -C ${DIR} add ../../WebUI/languages/DEFAULT.js
 git -C ${DIR} add ../../ext/Shared/Languages/DEFAULT.lua
