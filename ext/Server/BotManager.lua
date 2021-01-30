dc41b9217f86dea90cd9fead77dc72bbfb873743
Joe91 <1015.5@gmx.de>
2021-01-30 12:37:09 +0100

    let melee still count as ticket.

--- ext/Server/BotManager.lua
+++ ext/Server/BotManager.lua
@@ -203,7 +203,7 @@ function BotManager:_onSoldierDamage(hook, soldier, info, giverInfo)
 					info.direction = soldier.worldTransform.trans - bot.player.soldier.worldTransform.trans
 					info.origin = bot.player.soldier.worldTransform.trans
 					if (soldier.health - info.damage) < 0 then
-						if Globals.isTdm and not Config.useShotgun then
+						if Globals.isTdm then
 							local enemyTeam = TeamId.Team1;
 							if soldier.player.teamId == TeamId.Team1 then
 								enemyTeam = TeamId.Team2;
@@ -216,7 +216,7 @@ function BotManager:_onSoldierDamage(hook, soldier, info, giverInfo)
 				--valid bot-damage?
 				bot = self:GetBotByName(giverInfo.giver.name)
 				if bot ~= nil then
-					-- giver was a bot
+					-- giver was a bot (with shotgun)
 					if Config.useShotgun then
 						info.damage = Config.bulletDamageBotShotgun;
 						info.boneIndex = 0;
