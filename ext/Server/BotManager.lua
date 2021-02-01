227279a54914b4d338a180e0c613d58ad13418d4
Joe91 <1015.5@gmx.de>
2021-02-01 08:23:45 +0100

    use the real damage for Frag-Mode

--- ext/Server/BotManager.lua
+++ ext/Server/BotManager.lua
@@ -223,9 +223,9 @@ function BotManager:_onSoldierDamage(hook, soldier, info, giverInfo)
 				if bot ~= nil then
 					-- giver was a bot (with shotgun)
 					if Config.useShotgun then
-						--info.damage = info.damage * Config.damageFactor;
-						info.damage = bot.activeWeapon.damage * Config.damageFactor;
-						info.boneIndex = 0;
+						info.damage = info.damage * Config.damageFactor;
+						--info.damage = bot.activeWeapon.damage * Config.damageFactor;
+						--info.boneIndex = 0;
 					end
 				end
 			end
