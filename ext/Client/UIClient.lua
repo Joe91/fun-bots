29b77c3d8ecb61c30be9f33a3bcd53d30df0fd48
Joe91 <1015.5@gmx.de>
2021-02-04 09:07:11 +0100

    default also in UI

--- ext/Client/UIClient.lua
+++ ext/Client/UIClient.lua
@@ -74,15 +74,15 @@ function FunBotUIClient:_onUISettings(data)
 	settings:addList("GLOBAL", "botKit", Language:I18N("Bot Kit"), BotKits, data.botKit, "RANDOM_KIT", Language:I18N("The Kit a bots spawns with."));
 	settings:addList("GLOBAL", "botColor", Language:I18N("Bot Color"), BotColors, data.botColor, "RANDOM_COLOR", Language:I18N("The Kit-Color a bots spawns with."));
 	
-	settings:add("DIFFICULTY", "Float", "botAimWorsening", Language:I18N("Aim Worsening"), data.botAimWorsening, 0.0, Language:I18N("0 = hard, 1 (or higher) = easy. Only takes effect on level Start"));
+	settings:add("DIFFICULTY", "Float", "botAimWorsening", Language:I18N("Aim Worsening"), data.botAimWorsening, 0.2, Language:I18N("0 = hard, 1 (or higher) = easy. Only takes effect on level Start"));
 	settings:add("DIFFICULTY", "Float", "botSniperAimWorsening", Language:I18N("Aim Worsening Sniper"), data.botSniperAimWorsening, 0.0, Language:I18N("0 = hard, 1 (or higher) = easy. Only takes effect on level Start"));
-	settings:add("DIFFICULTY", "Float", "damageFactorAssault", Language:I18N("Factor for Assault-Weapon-Damage"), data.damageFactorAssault, 1.0, Language:I18N("Origninal Damage from bots gets multiplied by this"));
-	settings:add("DIFFICULTY", "Float", "damageFactorCarabine", Language:I18N("Factor for Carabine-Weapon-Damage"), data.damageFactorCarabine, 1.0, Language:I18N("Origninal Damage from bots gets multiplied by this"));
-	settings:add("DIFFICULTY", "Float", "damageFactorLMG", Language:I18N("Factor for LMG-Weapon-Damage"), data.damageFactorLMG, 1.0, Language:I18N("Origninal Damage from bots gets multiplied by this"));
-	settings:add("DIFFICULTY", "Float", "damageFactorSniper", Language:I18N("Factor for Sniper-Weapon-Damage"), data.damageFactorSniper, 1.0, Language:I18N("Origninal Damage from bots gets multiplied by this"));
-	settings:add("DIFFICULTY", "Float", "damageFactorShotgun", Language:I18N("Factor for Shotgun-Weapon-Damage"), data.damageFactorShotgun, 1.0, Language:I18N("Origninal Damage from bots gets multiplied by this"));
-	settings:add("DIFFICULTY", "Float", "damageFactorPistol", Language:I18N("Factor for Pistol-Weapon-Damage"), data.damageFactorPistol, 1.0, Language:I18N("Origninal Damage from bots gets multiplied by this"));
-	settings:add("DIFFICULTY", "Float", "damageFactorKnife", Language:I18N("Factor for Knife-Weapon-Damage"), data.damageFactorKnife, 1.0, Language:I18N("Origninal Damage from bots gets multiplied by this"));
+	settings:add("DIFFICULTY", "Float", "damageFactorAssault", Language:I18N("Factor for Assault-Weapon-Damage"), data.damageFactorAssault, 0.8, Language:I18N("Origninal Damage from bots gets multiplied by this"));
+	settings:add("DIFFICULTY", "Float", "damageFactorCarabine", Language:I18N("Factor for Carabine-Weapon-Damage"), data.damageFactorCarabine, 0.8, Language:I18N("Origninal Damage from bots gets multiplied by this"));
+	settings:add("DIFFICULTY", "Float", "damageFactorLMG", Language:I18N("Factor for LMG-Weapon-Damage"), data.damageFactorLMG, 0.8, Language:I18N("Origninal Damage from bots gets multiplied by this"));
+	settings:add("DIFFICULTY", "Float", "damageFactorSniper", Language:I18N("Factor for Sniper-Weapon-Damage"), data.damageFactorSniper, 0.8, Language:I18N("Origninal Damage from bots gets multiplied by this"));
+	settings:add("DIFFICULTY", "Float", "damageFactorShotgun", Language:I18N("Factor for Shotgun-Weapon-Damage"), data.damageFactorShotgun, 0.8, Language:I18N("Origninal Damage from bots gets multiplied by this"));
+	settings:add("DIFFICULTY", "Float", "damageFactorPistol", Language:I18N("Factor for Pistol-Weapon-Damage"), data.damageFactorPistol, 0.8, Language:I18N("Origninal Damage from bots gets multiplied by this"));
+	settings:add("DIFFICULTY", "Float", "damageFactorKnife", Language:I18N("Factor for Knife-Weapon-Damage"), data.damageFactorKnife, 0.8, Language:I18N("Origninal Damage from bots gets multiplied by this"));
 
 	settings:add("SPAWN", "Boolean", "spawnOnLevelstart", Language:I18N("Spawn on Levelstart"), data.spawnOnLevelstart, true, Language:I18N("Bots spawn on levelstart (if valid paths are available)"));
 	settings:add("SPAWN", "Boolean", "onlySpawnBotsWithPlayers", Language:I18N("Only spawn with players"), data.onlySpawnBotsWithPlayers, true, Language:I18N("Bots only spawn if at least one Player is on the server"));
