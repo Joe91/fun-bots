53c51e97374e48676a9d4cc57a6c18a7274dd9c7
Joe91 <1015.5@gmx.de>
2021-02-16 19:38:13 +0100

    use only two points...

--- ext/Server/Bot.lua
+++ ext/Server/Bot.lua
@@ -630,23 +630,16 @@ function Bot:_updateMovement()
 			if Globals.wayPoints[self._pathIndex][1] ~= nil then -- check for reached point
 				local point				= nil;
 				local nextPoint			= nil;
-				local nextNextPoint		= nil;
 				local pointIncrement	= 1;
 				local useShootWayPoint	= false;
 
 				if #self._shootWayPoints > 0 then	--we need to go back to path first
 					point 				= self._shootWayPoints[#self._shootWayPoints];
 					nextPoint 			= self._shootWayPoints[#self._shootWayPoints - 1];
-					nextNextPoint		= self._shootWayPoints[#self._shootWayPoints - 2];
 					if nextPoint == nil then
 						nextPoint = Globals.wayPoints[self._pathIndex][activePointIndex];
-						NetEvents:BroadcastLocal('ClientNodeEditor:BotSelect', self._pathIndex, activePointIndex, self.player.soldier.worldTransform.trans, (self._obstaceSequenceTimer > 0), "Blue")
-					end
-					if nextNextPoint == nil then
-						if not self._invertPathDirection then
-							nextPoint = Globals.wayPoints[self._pathIndex][self:_getWayIndex(self._currentWayPoint + 1)]
-						else
-							nextPoint = Globals.wayPoints[self._pathIndex][self:_getWayIndex(self._currentWayPoint - 1)]
+						if Config.debugTracePaths then
+							NetEvents:BroadcastLocal('ClientNodeEditor:BotSelect', self._pathIndex, activePointIndex, self.player.soldier.worldTransform.trans, (self._obstaceSequenceTimer > 0), "Blue")
 						end
 					end
 					useShootWayPoint	= true;
@@ -654,12 +647,14 @@ function Bot:_updateMovement()
 					point = Globals.wayPoints[self._pathIndex][activePointIndex];
 					if not self._invertPathDirection then
 						nextPoint 		= Globals.wayPoints[self._pathIndex][self:_getWayIndex(self._currentWayPoint + 1)]
-						nextNextPoint 	= Globals.wayPoints[self._pathIndex][self:_getWayIndex(self._currentWayPoint + 2)]
-						NetEvents:BroadcastLocal('ClientNodeEditor:BotSelect', self._pathIndex, self:_getWayIndex(self._currentWayPoint + 1), self.player.soldier.worldTransform.trans, (self._obstaceSequenceTimer > 0), "Green")
+						if Config.debugTracePaths then
+							NetEvents:BroadcastLocal('ClientNodeEditor:BotSelect', self._pathIndex, self:_getWayIndex(self._currentWayPoint + 1), self.player.soldier.worldTransform.trans, (self._obstaceSequenceTimer > 0), "Green")
+						end
 					else
 						nextPoint 		= Globals.wayPoints[self._pathIndex][self:_getWayIndex(self._currentWayPoint - 1)]
-						nextNextPoint	= Globals.wayPoints[self._pathIndex][self:_getWayIndex(self._currentWayPoint - 2)]
-						NetEvents:BroadcastLocal('ClientNodeEditor:BotSelect', self._pathIndex, self:_getWayIndex(self._currentWayPoint + 1), self.player.soldier.worldTransform.trans, (self._obstaceSequenceTimer > 0), "Green")
+						if Config.debugTracePaths then
+							NetEvents:BroadcastLocal('ClientNodeEditor:BotSelect', self._pathIndex, self:_getWayIndex(self._currentWayPoint - 1), self.player.soldier.worldTransform.trans, (self._obstaceSequenceTimer > 0), "Green")
+						end
 					end
 				end
 
@@ -670,22 +665,22 @@ function Bot:_updateMovement()
 					if Config.overWriteBotSpeedMode > 0 then
 						self.activeSpeedValue = Config.overWriteBotSpeedMode;
 					end
-					local dy					= nextPoint.trans.z - self.player.soldier.worldTransform.trans.z;
-					local dx					= nextPoint.trans.x - self.player.soldier.worldTransform.trans.x;
+					local dy					= point.trans.z - self.player.soldier.worldTransform.trans.z;
+					local dx					= point.trans.x - self.player.soldier.worldTransform.trans.x;
 					local distanceFromTarget	= math.sqrt(dx ^ 2 + dy ^ 2);
-					local heightDistance		= math.abs(nextPoint.trans.y - self.player.soldier.worldTransform.trans.y);
+					local heightDistance		= math.abs(point.trans.y - self.player.soldier.worldTransform.trans.y);
 
 
 					--detect obstacle and move over or around TODO: Move before normal jump
-					local currentWayPontDistance = self.player.soldier.worldTransform.trans:Distance(nextPoint.trans);
+					local currentWayPontDistance = self.player.soldier.worldTransform.trans:Distance(point.trans);
 					if currentWayPontDistance > self._lastWayDistance + 0.02 and self._obstaceSequenceTimer == 0 then
 						--TODO: skip one pooint?
 						distanceFromTarget			= 0;
 						heightDistance				= 0;
 					end
 
-					self._targetPoint = nextPoint;
-					self._nextTargetPoint = nextNextPoint;
+					self._targetPoint = point;
+					self._nextTargetPoint = nextPoint;
 
 
 					if math.abs(currentWayPontDistance - self._lastWayDistance) < 0.02 or self._obstaceSequenceTimer ~= 0 then
@@ -752,7 +747,7 @@ function Bot:_updateMovement()
 
 					-- jump detection. Much more simple now, but works fine ;-)
 					if self._obstaceSequenceTimer == 0 then
-						if (nextPoint.trans.y - self.player.soldier.worldTransform.trans.y) > 0.3 and Config.jumpWhileMoving then
+						if (point.trans.y - self.player.soldier.worldTransform.trans.y) > 0.3 and Config.jumpWhileMoving then
 							--detect, if a jump was recorded or not
 							local timeForwardBackwardJumpDetection = 1.1; -- 1.5 s ahead and back
 							local jumpValid = false;
