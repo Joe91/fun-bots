8460cd0d169d39b263a7195dae5eba26347d8a62
Joe91 <1015.5@gmx.de>
2021-02-22 07:35:30 +0100

    information seems to be correct. Only point cannot be extracted

--- ext/Server/PathSwitcher.lua
+++ ext/Server/PathSwitcher.lua
@@ -7,14 +7,20 @@ function PathSwitcher:__init()
 end
 
 function PathSwitcher:getNewPath(data, currentPath, currentPoint, objective)
-	if data ~= nil and data.links ~= nil then
-		print("data")
-		--if data.LinkMode >= 0 then -- random PathSwitcher
+	if data ~= nil and data.Links ~= nil and #data.Links > 0 then
+		if data.LinkMode >= 0 then -- random PathSwitcher
 			if MathUtils:GetRandomInt(1, 100) > 0 then -- only swith every time :-)
-				local point = g_NodeCollection.waypointsByID[data.links[MathUtils:GetRandomInt(1,#data.links)]]
-				return true, point.PathIndex, point.PointIndex;
+				local link = data.Links[MathUtils:GetRandomInt(1, #data.Links)];
+				print(link[2])
+				local point = g_NodeCollection.waypointsByID[link[2]]
+				if point~= nil then
+					print("switch done")
+					return true, point.PathIndex, point.PointIndex;
+				else
+					print("point not available")
+				end
 			end
-		--end
+		end
 	end
 	return false;
 end
