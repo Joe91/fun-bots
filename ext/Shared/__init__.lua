131d1fd59f6c2c84e52a66d048856d2dd58b014c
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-03-07 16:08:12 +0100

    Fix Debug on Update

--- ext/Shared/__init__.lua
+++ ext/Shared/__init__.lua
@@ -14,14 +14,18 @@ end
 function FunBotShared:OnUpdateCheck()
 	print('fun-bots ' .. VERSION .. ' (' .. BRANCH .. ')');
 	
-	if Debug.Globals.Globals then
+	if Debug.Globals.UPDATE then
 		print('Checking for Updates...');
 	end
 	
 	local response	= Net:GetHTTP('https://api.github.com/repos/Joe91/fun-bots/releases?per_page=1');
 	local json		= json.decode(response.body);
 	
-	if 'V' .. VERSION ~= json[1].name then
+	if json == nil or json[1] == nil then
+		if Debug.Globals.UPDATE then
+			print('Can\'t fetch the latest Version from GitHub.');
+		end
+	elseif 'V' .. VERSION ~= json[1].name then
 		local isOlderVersion	= false
 		local currentV			= VERSION:split('.')
 		local latestV			= json[1].name:sub(2):split('.')
@@ -39,10 +43,10 @@ function FunBotShared:OnUpdateCheck()
 			print('Installed Version: V' .. VERSION);
 			print('Online Version: ' .. json[1].name);
 			print('++++++++++++++++++++++++++++++++++++++++++++++++++');
-		elseif Debug.Globals.Globals then
+		elseif Debug.Globals.UPDATE then
 			print('You have already the newest version installed.');
 		end
-	elseif Debug.Globals.Globals then
+	elseif Debug.Globals.UPDATE then
 		print('You have already the newest version installed.');
 	end
 	
