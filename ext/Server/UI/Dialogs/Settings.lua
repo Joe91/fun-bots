83c97faa5379fd004c38b618fb900eedac13a8b9
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-04-09 18:47:34 +0200

    Update Settings as an Dialog

--- ext/Server/UI/Dialogs/Settings.lua
+++ ext/Server/UI/Dialogs/Settings.lua
@@ -1,19 +1,37 @@
 class('Settings')
 
 function Settings:__init()
-	self:InitializeComponent();
+	self.dialog = Dialog('Settings');
 end
 
 function Settings:InitializeComponent()
-	-- Create Dialog
+	print('Init Settings');
+	
 	-- Add Menu
+	
 	-- Add Buttons
+	self.dialog:AddButton(Button('button_settings_cancel', 'Cancel', function(player)
+		print('[Settings] Button Cancel');
+	end), Position.Left);
+	
+	self.dialog:AddButton(Button('button_settings_restore', 'Restore all to Default', function(player)
+		print('[Settings] Button Restore');
+	end), Position.Left, 'Settings.Restore');
+	
+	self.dialog:AddButton(Button('button_settings_save_temporarily', 'Save Temporarily', function(player)
+		print('[Settings] Button Temporarily');
+	end), Position.Right, 'Settings.Save');
+	
+	self.dialog:AddButton(Button('button_settings_save', 'Save', function(player)
+		print('[Settings] Button Save');
+	end), Position.Right, 'Settings.Save');
+	
 	-- Add Content
 		-- Add Tabs
 end
 
 function Settings:Open(view, player)
-	view:Push(player, Alert(Position.Bottom_Center, Color.Red, 'Blah, Blah, Blah!', 2500));
+	view:Push(player, self.dialog);
 end
 
 return Settings;
