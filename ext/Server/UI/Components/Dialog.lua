667e846f47036bb65eacc54ea59a69c83703766b
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-04-09 18:46:32 +0200

    Create Dialog component

--- ext/Server/UI/Components/Dialog.lua
+++ ext/Server/UI/Components/Dialog.lua
@@ -1,15 +1,49 @@
 class('Dialog');
 
-function Dialog:__init()
-	
+function Dialog:__init(title)
+	self.title		= title or nil;
+	self.buttons	= {};
+	self.content	= nil;
 end
 
 function Dialog:__class()
 	return 'Dialog';
 end
 
-function Dialog:Serialize()
-	return '';
+function Dialog:AddButton(button, position, permission)
+	if (button == nil or button['__class'] == nil) then
+		-- Bad Item
+		return;
+	end
+	
+	if (button:__class() ~= 'Button') then
+		-- Exception: Only Button
+		return;
+	end
+	
+	if position ~= nil then
+	
+	end
+	
+	if permission ~= nil then
+		button:BindPermission(permission);
+	end
+	
+	print(button:Serialize());
+	
+	table.insert(self.buttons, button);
+end
+
+function Dialog:SetContent(content)
+	self.content = content;
+end
+
+function Dialog:Serialize(player)
+	return {
+		Title	= self.title,
+		Content = self.content,
+		Buttons	= self.buttons
+	};
 end
 
 return Dialog;
