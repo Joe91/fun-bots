0aaae9b916f5399ab0b3640b0d8435d12f6c875b
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2021-04-09 18:45:51 +0200

    Add Button component

---
+++ ext/Server/UI/Components/Button.lua
@@ -0,0 +1,40 @@
+class('Button');
+
+function Button:__init(name, title, callback)
+	self.name		= name or nil;
+	self.title		= title or nil;
+	self.callback	= callback or nil;
+	self.permission	= nil;
+	self.disabled	= false;
+end
+
+function Button:__class()
+	return 'Button';
+end
+
+function Button:GetPermission()
+	return self.permission;
+end
+
+function Button:BindPermission(permission)
+	self.permission = permission;
+end
+
+function Button:Enable()
+	self.disabled = false;
+end
+
+function Button:Disable()
+	self.disabled = true;
+end
+
+function Button:Serialize()
+	return {
+		Name		= self.name,
+		Title		= self.title,
+		Disabled	= self.disabled,
+		Permission	= self.permission
+	};
+end
+
+return Button;
