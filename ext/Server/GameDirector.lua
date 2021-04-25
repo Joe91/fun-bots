06b39cf6ccf2bc73af5d8488674dc9a9380f41ad
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-04-25 18:47:46 +0200

    register only needed event callbacks
    will probably remove the Events:Dispatch and change to:
    `s_Entity:RegisterEventCallback(self, self.OnMCOMArmed)` etc.

--- ext/Server/GameDirector.lua
+++ ext/Server/GameDirector.lua
@@ -27,23 +27,27 @@ function GameDirector:onLevelLoaded()
 		self.McomCounter = 0
 		self.ArmedMcoms = {}
 
-		local mcomIterator = EntityManager:GetIterator("EventSplitterEntity")
-		local mcomEntity = mcomIterator:Next()
-
-		while mcomEntity do
-			mcomEntity = Entity(mcomEntity)
-			mcomEntity:RegisterEventCallback(function(ent, entityEvent)
-				if ent.data.instanceGuid == Guid("87E78B77-78F9-4DE0-82FF-904CDC2F7D03") then
-					Events:Dispatch('MCOM:Armed', entityEvent.player)
-				end
-				if ent.data.instanceGuid == Guid("74B7AD6D-8EB5-40B1-BB53-C0CFB956048E") then
-					Events:Dispatch('MCOM:Disarmed', entityEvent.player)
-				end
-				if ent.data.instanceGuid == Guid("70B36E2F-0B6F-40EC-870B-1748239A63A8") then
-					Events:Dispatch('MCOM:Destroyed', entityEvent.player)
-				end
-			end)
-			mcomEntity = mcomIterator:Next()
+		local s_Iterator = EntityManager:GetIterator("EventSplitterEntity")
+		local s_Entity = s_Iterator:Next()
+		while s_Entity do
+			if s_Entity.data == nil then
+				goto continue_entity_loop
+			end
+			if s_Entity.data.instanceGuid == Guid("87E78B77-78F9-4DE0-82FF-904CDC2F7D03") then
+				s_Entity:RegisterEventCallback(function(p_Entity, p_EntityEvent)
+					Events:Dispatch('MCOM:Armed', p_EntityEvent.player)
+				end)
+			elseif s_Entity.data.instanceGuid == Guid("74B7AD6D-8EB5-40B1-BB53-C0CFB956048E") then
+				s_Entity:RegisterEventCallback(function(p_Entity, p_EntityEvent)
+					Events:Dispatch('MCOM:Disarmed', p_EntityEvent.player)
+				end)
+			elseif s_Entity.data.instanceGuid == Guid("70B36E2F-0B6F-40EC-870B-1748239A63A8") then
+				s_Entity:RegisterEventCallback(function(p_Entity, p_EntityEvent)
+					Events:Dispatch('MCOM:Destroyed', p_EntityEvent.player)
+				end)
+			end
+			::continue_entity_loop::
+			s_Entity = s_Iterator:Next()
 		end
 	end
 
