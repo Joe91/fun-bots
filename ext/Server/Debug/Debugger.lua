41e95304a4a45e329bfeb54f2c9533fadc130e69
Firjen <57008952+Firjens@users.noreply.github.com>
2021-08-20 00:24:08 +0200

    Added Timestamp and modified Debuger

--- ext/Server/Debug/Debugger.lua
+++ ext/Server/Debug/Debugger.lua
@@ -1,92 +1,117 @@
 class('Debugger')
 
--- Basic debugger
+-- Create a bug report using an in-game !bugreport command.
+-- A bug report is required in order to create a new issue on the Github.
 -- Author: @Firjen on 12/07/21
+-- Debugger version: 1
 
 -- The URL to the fun-bots report site
 local DEBUG_REPORT_URL = "https://report.funbots.dev"
 
+-- Pre-check path (checks if a report can be made)
 local DEBUG_ELIGIABLE_PATH = "/api/precheck"
 
+-- Path to submit a bug report
 local DEBUG_SUBMIT_PATH = "/api/submit"
 
-local pp = nil
+-- Timestamp of the latest report
+local DEBUG_LAST_REPORT = 0
 
-local s_CheckURL = "https://report.funbots.dev/api/precheck?uuid=a5bb3716-1768-4367-b1ba-6b6ab0b7fbb2"
+-- Cooldown between bug reports in milliesconds
+local DEBUG_REPORT_COOLDOWN = 60*1000*2.5 -- 2.5 minutes cooldown
 
-function TestGetHTTPAsync()
+-- Ran with the !bugreport command.
+-- p_Player - User who initiated the request
+function Debugger:GenerateReport(p_Player)
+    -- Check cooldown
+    if DEBUG_LAST_REPORT ~= nil and (DEBUG_LAST_REPORT - SharedUtils:GetTimeMS() > 0) then 
+        ChatManager:Yell("A report was recently made, please wait " .. ReadableTimetamp((DEBUG_LAST_REPORT-SharedUtils:GetTimeMS()), TimeUnits.FIT, 1) .. " before creating a new report", 5.0, p_Player)
+        do return end
+    end
+
+    -- Set the cooldown
+    DEBUG_LAST_REPORT = SharedUtils:GetTimeMS() + DEBUG_REPORT_COOLDOWN
+
+    -- Check that the server GUID is set (required)
+    if RCON:GetServerGuid() == nil then
+        ChatManager:Yell("Failed to create bug report. Server GUID is null", 5.0, p_Player)
+        print("[Debugger: Report] Failure to create a bug report. Server GUID is null.");
+        print("[Debugger: Report] This is not a fun-bots bug, please contact Venice Unleashed.");
+        do return end
+    end
+
+    -- Create a pre-check check.
+    print("[Debugger: Report] " .. p_Player.name .. " is creating a new bug report.");
+    ChatManager:Yell("Creating a new bug report...", 5.0, p_Player) -- Yell gets overwritten by a newer yell if available.
+
+    local s_CheckURL = DEBUG_REPORT_URL .. DEBUG_ELIGIABLE_PATH .. "?uuid=" .. RCON:GetServerGuid():ToString("D");
     Net:GetHTTPAsync(s_CheckURL, function(p_HttpResponse)
         if p_HttpResponse == nil then
-            print("[TestGetHTTPAsync] p_HttpResponse == nil")
+            ChatManager:Yell("Failed to create bug report. Unable to contact report server.", 5.0, p_Player)
+            print("[Debugger: Report] Failure to precheck for a bug report. Unable to contact report server. Please check your internet connection and try again later.");
+            do return end
         end
 
+		local s_Json = json.decode(p_HttpResponse.body)
+
+        -- Too many requests, try again later
+        if p_HttpResponse.status == 429 then 
+            ChatManager:Yell("Too many bug reports created, try again in " .. ReadableTimetamp(s_Json.try_again, TimeUnits.FIT, 1), 5.0, p_Player)
+            print("[Debugger: Report] Failure to precheck for a bug report. Too many bug reports created, try again in " .. ReadableTimetamp(s_Json.try_again, TimeUnits.FIT, 0));
+            do return end
+        end
+
+        -- Not cooldown and not 200
         if p_HttpResponse.status ~= 200 then
-            print("[TestGetHTTPAsync] p_HttpResponse.status ~= 200")
+            ChatManager:Yell("Failed to create bug report. Returned HTTP code " .. p_HttpResponse.status, 5.0, p_Player)
+            print("[Debugger: Report] Failure to precheck for a bug report. Returned HTTP code " .. p_HttpResponse.status);
+            do return end
         end
 
-        local s_Json = json.decode(p_HttpResponse.body)
 
-        print("[TestGetHTTPAsync]")
-        print(s_Json)
+        -- Report can be created as server returned error 200.
+        local s_SubmitURL = DEBUG_REPORT_URL .. DEBUG_SUBMIT_PATH .. "?uuid=" .. RCON:GetServerGuid():ToString("D");
+
+        -- Parse JSON data to POST
+        local s_ReportData = {
+            config = Config
+        }
+
+        --[[
+            
+            author_name = p_Player.name,
+            author_guid = p_Player.guid,
+            map_id = SharedUtils:GetLevelName(),
+            gamemode_id = SharedUtils:GetCurrentGameMode(),
+            tps = SharedUtils:GetTickrate(),
+            players_online = PlayerManager.GetPlayerCount()
+        ]]
+
+        Net:PostHTTPAsync(s_SubmitURL, json.encode(s_ReportData), function(p_HttpResponse)
+            if p_HttpResponse == nil then
+                ChatManager:Yell("Failed to create bug report. Unable to contact report server.", 5.0, p_Player)
+                print("[Debugger: Report] Failure to post for a bug report. Unable to contact report server. Please check your internet connection and try again later.");
+                do return end
+            end
+
+            -- Not cooldown and not 200
+            if p_HttpResponse.status ~= 200 then
+                ChatManager:Yell("Failed to create bug report. Returned HTTP code " .. p_HttpResponse.status, 5.0, p_Player)
+                print("[Debugger: Report] Failure to post a bug report. Returned HTTP code " .. p_HttpResponse.status);
+                do return end
+            end
+
+            print(p_HttpResponse.status)
+            print(p_HttpResponse.body)
+        end)
+
     end)
-end
-
--- Generate a bug report and send it to the fun-bots bug report server
-function Debugger:GenerateReport(p_Player)
-   TestGetHTTPAsync();
-end
-
-function GenerateReportCallback(httpRequest)
-    -- @Todo: Figure out how to pass this variable with GenerateReportCallback
-    local p_Player = pp
-
-    if httpRequest == nil then
-        print("[DEBUG] Bug report failed: HTTP request failure")
-        ChatManager:Yell("Failed to create bug report. HTTP request failure",  4.0, pp)
-        do return end
-    end
-
-    -- Check code returned
-    if httpRequest.status == 429 then 
-        print("[DEBUG] Bug report failed: too many bug reports created. You can only create 2 reports per 24 hr.")
-        ChatManager:Yell("Failed to create bug report. Report limit reached (max 2 per 24hr)", 4.0, p_Player)
-        do return end
-    elseif httpRequest.status ~= 429 and httpRequest.status ~= 200  then
-        print("[DEBUG] Bug report failed: Report server returned error " .. httpRequest.status)
-        ChatManager:Yell("Failed to create bug report. HTTP code " .. httpRequest.status, 4.0, p_Player)
-        do return end
-    end
-
-    -- If code 200, we can ask the server to create our report.
-    Debugger:CreateReport(p_Player)
-end
-
--- Generate the report
-function Debugger:CreateReport(p_Player)
-    -- Generate POST data
-    local postData = {config = json.encode(Config)}
-    print(postData)
-
-    local s_String = "config=" .. json.encode(Config) .. "&version=2.2.0" .. "author=" .. p_Player.name
-
-    -- Send post data
-    Net:PostHTTPAsync(DEBUG_REPORT_URL .. DEBUG_REPORT_URL .. "?uuid=" .. RCON:GetServerGuid(), json.encode(Config), CreateReportCallback)
-end
-
-function CreateReportCallback(httpRequest)
-    if httpRequest == nil then
-        print("[DEBUG] Bug report failed: HTTP request failure")
-        ChatManager:Yell("Failed to create bug report. HTTP request failure 2",  4.0, pp)
-        do return end
-    end
-    
-    ChatManager:Yell("Bug report created?",  4.0, pp)
-end
+ end
 
 function Debugger:__init()
-    local s_start = os.time(os.date("!*t"))
+    local s_start = SharedUtils:GetTimeMS()
 
-	print("Loaded debugger v2 in " .. os.time(os.date("!*t")) - s_start .. " ms")
+	print("Loaded debugger v2 in " .. ReadableTimetamp(os.time(os.date("!*t")) - s_start, TimeUnits.FIT, 1) .. " ms")
 
     return true
 end
--- ext/Shared/Utils/Timestamp.lua
+++ ext/Shared/Utils/Timestamp.lua
@@ -13,3 +13,63 @@ function ParseOffset(p_String)
 
 	return s_Timestamp + s_Offset
 end
+
+-- Enums for different time units used to convert milliseconds into a readable format to the clients.
+-- Author: Firjen (19/08/21)
+TimeUnits = {
+	FIT = 99,
+	DAYS = 4,
+	HOURS = 3,
+	MINUTES = 2,
+	SECONDS = 1,
+}
+
+-- Trim a large number to a smaller number with specified degree (numbers behind the comma)
+-- Param: degree - Total numbers 
+-- Param: integer - number to trim 
+-- Author: Firjen (19/08/21)
+-- Return: String [Double-like] (eg. 2.1, 1.6, 0.5)
+function Trim(degree, integer)
+    return string.format("%.1f",integer)
+end
+
+-- Convert to a readable format (eg. 5 Minutes, 12 Seconds, etc.)
+-- Author: Firjen (19/08/21)
+-- Return: String (Eg. Permanent, 12 Seconds, 5 Minutes)
+function ReadableTimetamp(p_Time, p_TimeUnit, p_Trim)
+	-- When time is equal to 0 it's now
+	if p_Time == 0 then
+		return "Now"
+	end
+
+	-- Anything less than 0 (eg. time is -1), it's considered permanent
+	if p_Time < 0 then
+		return "Permanent"
+	end
+
+	-- If fit pick the best timeunit
+	if p_TimeUnit == TimeUnits.FIT then
+		if p_Time < 60000 then
+			p_TimeUnit = TimeUnits.SECONDS
+		elseif p_Time < 3600000 then
+			p_TimeUnit = TimeUnits.MINUTES
+		elseif p_Time < 86400000 then
+			p_TimeUnit = TimeUnits.HOURS
+		else
+			p_TimeUnit = TimeUnits.DAYS
+		end
+	end		
+	
+
+	if p_TimeUnit == TimeUnits.DAYS then
+		return Trim(p_Trim, p_Time / 8.64E7) .. " Days"
+	elseif p_TimeUnit == TimeUnits.HOURS then
+		return Trim(p_Trim, p_Time / 3600000) .. " Hours"
+	elseif p_TimeUnit == TimeUnits.MINUTES then
+		return Trim(p_Trim, p_Time / 86400000) .. " Minutes"
+	elseif p_TimeUnit == TimeUnits.SECONDS then
+		return Trim(p_Trim, p_Time / 1000) .. " Seconds"
+	else
+		return Trim(p_Trim, p_Time) .. " Milliseconds"
+	end
+end
