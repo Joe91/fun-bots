8734a63ea7155caa63b8479186771204eec57048
Firjen <firjen@plontivion.com>
2021-08-28 20:12:07 +0200

    Update CompileUI.bat

--- dev/CompileUI.bat
+++ dev/CompileUI.bat
@@ -1,9 +1,9 @@
 @echo off
 
 rem Compile for Source
-vuicc.exe "..\WebUI" "..\ui.vuic"
+"..\.funbots\workflow\ui_generator\vuicc.exe" "..\WebUI" "..\ui.vuic"
 
 rem Compile for Client
-vuicc.exe "..\WebUI" "%localappdata%\VeniceUnleashed\mods\fun-bots\ui.vuic"
+"..\.funbots\workflow\ui_generator\vuicc.exe" "..\WebUI" "%localappdata%\VeniceUnleashed\mods\fun-bots\ui.vuic"
 
 pause
