261b6b8588ed70f83894d07d8d3f6aad16d37a60
Joe91 <1015.5@gmx.de>
2021-10-07 22:53:56 +0200

    new try on the mcoms

--- ext/Shared/Registry/Registry.lua
+++ ext/Shared/Registry/Registry.lua
@@ -50,12 +50,6 @@ Registry = {
                 RAYCAST_INTERVAL = 0.05,
         },
 
-		GAME_DIRECTOR = {
-				UPDATE_OBJECTIVES_CYCLE = 1.5,
-
-				MCOMS_CHECK_CYCLE = 26.0,
-		},
-
         -- Bot related
         BOT = {
                 -- Bot attack bot check interval
