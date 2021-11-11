30de159c8279802066201503c311e2ac1a1a91a8
Joe91 <1015.5@gmx.de>
2021-11-11 20:29:03 +0100

    update mod.db

--- dev/create_mapfiles.bat
+++ dev/create_mapfiles.bat
@@ -2,5 +2,5 @@ python scan_for_invalid_objectives.py
 python create_maplist.py
 python update_supported_maps.py
 python clear_settings.py
-call "./../import_traces.exe"
+call ".\..\import_traces.exe"
 pause
