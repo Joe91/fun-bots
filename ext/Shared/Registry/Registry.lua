c8cef14e9db87d1f99260b2adc46fc6e14943224
Joe91 <1015.5@gmx.de>
2022-02-16 19:21:54 +0100

    bring some changes into master

--- ext/Shared/Registry/Registry.lua
+++ ext/Shared/Registry/Registry.lua
@@ -35,7 +35,7 @@ Registry = {
 			-- Patch version
 			VERSION_PATCH = 0,
 			-- Additional label for pre-releases and build meta data
-			VERSION_LABEL = "dev4",
+			VERSION_LABEL = "RC1",
 			-- Current version type of this build
 			VERSION_TYPE = VersionType.DevBuild,
 			-- The Version used for the Update-Check
