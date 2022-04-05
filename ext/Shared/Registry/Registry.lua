737c0ee3b71bf651efb7efecc2bfc5d744e150da
Joe91 <1015.5@gmx.de>
2022-04-05 10:22:18 +0200

    create new release-Version

--- ext/Shared/Registry/Registry.lua
+++ ext/Shared/Registry/Registry.lua
@@ -39,11 +39,11 @@ Registry = {
 			-- Patch version
 			VERSION_PATCH = 0,
 			-- Additional label for pre-releases and build meta data
-			VERSION_LABEL = "RC3",
+			VERSION_LABEL = "",
 			-- Current version type of this build
-			VERSION_TYPE = VersionType.DevBuild,
+			VERSION_TYPE = VersionType.Release,
 			-- The Version used for the Update-Check
-			UPDATE_CHANNEL = VersionType.DevBuild,
+			UPDATE_CHANNEL = VersionType.Stable,
 			-- prints current version in console
 			CLIENT_SHOW_VERSION_ON_JOIN = false,
 		},
