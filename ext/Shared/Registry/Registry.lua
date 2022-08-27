99c75462e1508c4ab54f8f747f1dd8416c236601
Joe91 <1015.5@gmx.de>
2022-08-27 21:48:52 +0200

    increment version

--- ext/Shared/Registry/Registry.lua
+++ ext/Shared/Registry/Registry.lua
@@ -39,7 +39,7 @@ Registry = {
 		-- Patch version
 		VERSION_PATCH = 0,
 		-- Additional label for pre-releases and build meta data
-		VERSION_LABEL = "dev3",
+		VERSION_LABEL = "dev4",
 		-- Current version type of this build
 		VERSION_TYPE = VersionType.DevBuild,
 		-- The Version used for the Update-Check
