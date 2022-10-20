0380c4e1f81f33cc2a19483914c1c0aa949b9915
João Vitor <joaovitorsouza205@hotmail.com>
2022-10-19 21:39:39 -0300

    test: initial file

---
+++ dev/fun-bots-helper/tests/test_clear_settings.py
@@ -0,0 +1,27 @@
+"""Execute tools/clear_settings.py over a temporary copy of the mod database and test 
+if any of the remaining tables after clear settings is a setting table."""
+
+
+def test_clear_settings(session):
+    """Key arguments.
+
+    session: cursor object from a temporary copy of mod.db
+    """
+    removeList = ["FB_Config_Trace", "FB_Settings"]
+
+    for tablename in removeList:
+        print("Remove " + tablename)
+        session.execute("DROP TABLE IF EXISTS " + tablename)
+
+    session.execute(
+        """
+        SELECT name FROM sqlite_master WHERE type='table'
+    """
+    )
+
+    retrieved_tables = session.fetchall()
+
+    assert all(
+        table not in retrieved_tables
+        for table in [("FB_Config_Trace",), ("FB_Settings",)]
+    )
