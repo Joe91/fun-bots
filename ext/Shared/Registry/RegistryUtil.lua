5db454fa9cb39e7e843d77c461bda65a0d097275
João Vitor <joaovitorsouza205@hotmail.com>
2022-10-29 13:50:30 -0300

    fix: returns of get method

--- ext/Shared/Registry/RegistryUtil.lua
+++ ext/Shared/Registry/RegistryUtil.lua
@@ -1,6 +1,6 @@
 ---@class RegistryUtil
 ---@overload fun():RegistryUtil
-RegistryUtil = class 'RegistryUtil'
+RegistryUtil = class('RegistryUtil')
 
 require('__shared/Registry/Registry')
 
@@ -10,36 +10,34 @@ require('__shared/Registry/Registry')
 local MODULE_NAME = "Registry Manager: Util"
 
 function RegistryUtil:__init()
-	local s_start = SharedUtils:GetTimeMS()
-
-	print("Enabled \"" .. MODULE_NAME .. "\" in " .. ReadableTimetamp(SharedUtils:GetTimeMS() - s_start, TimeUnits.FIT, 1))
+    self._StartTime = SharedUtils:GetTimeMS()
+    print("Enabled \"" .. MODULE_NAME .. "\" in " ..
+              ReadableTimetamp(SharedUtils:GetTimeMS() - self._StartTime, TimeUnits.FIT, 1))
 end
 
 -- Get the version of the current build as in a semantic format.
 -- @return String - semantic version
 -- @author Firjen <https://github.com/Firjens>
 function RegistryUtil:GetVersion()
-	-- If there is no label, we return the MAJ.MIN.PATCH, otherwise we need
-	-- to return the MAJ.MIN.PATCH-LABEL.
-	if Registry.VERSION.VERSION_LABEL == nil or Registry.VERSION.VERSION_LABEL == "" or
-		Registry.VERSION.VERSION_TYPE == VersionType.Release then
-		return "V" ..
-			Registry.VERSION.VERSION_MAJ .. "." .. Registry.VERSION.VERSION_MIN .. "." .. Registry.VERSION.VERSION_PATCH;
-	else
-		if Registry.VERSION.VERSION_TYPE == VersionType.DevBuild then
-			return "v" ..
-				Registry.VERSION.VERSION_MAJ ..
-				"." .. Registry.VERSION.VERSION_MIN .. "." .. Registry.VERSION.VERSION_PATCH .. "-" .. Registry.VERSION.VERSION_LABEL;
-		elseif Registry.VERSION.VERSION_TYPE == VersionType.Stable then
-			return "V" ..
-				Registry.VERSION.VERSION_MAJ ..
-				"." .. Registry.VERSION.VERSION_MIN .. "." .. Registry.VERSION.VERSION_PATCH .. "-" .. Registry.VERSION.VERSION_LABEL;
-		end
-	end
+    -- If there is no label, we return the MAJ.MIN.PATCH, otherwise we need
+    -- to return the MAJ.MIN.PATCH-LABEL.
+    if Registry.VERSION.VERSION_LABEL == nil or Registry.VERSION.VERSION_LABEL == "" or Registry.VERSION.VERSION_TYPE ==
+        VersionType.Release then
+        return "V" .. Registry.VERSION.VERSION_MAJ .. "." .. Registry.VERSION.VERSION_MIN .. "." ..
+                   Registry.VERSION.VERSION_PATCH;
+    else
+        MAJ_MIN_PATCH_LABEL = Registry.VERSION.VERSION_MAJ .. "." .. Registry.VERSION.VERSION_MIN .. "." ..
+                                  Registry.VERSION.VERSION_PATCH .. "-" .. Registry.VERSION.VERSION_LABEL
+        if Registry.VERSION.VERSION_TYPE == VersionType.DevBuild then
+            return "v" .. MAJ_MIN_PATCH_LABEL
+        elseif Registry.VERSION.VERSION_TYPE == VersionType.Stable then
+            return "V" .. MAJ_MIN_PATCH_LABEL
+        end
+    end
 end
 
 if g_RegistryUtil == nil then
-	---@type RegistryUtil
+    ---@type RegistryUtil
 	g_RegistryUtil = RegistryUtil()
 end
 
