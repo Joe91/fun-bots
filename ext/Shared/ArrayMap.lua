5afd5d0be6e3ea045a0406b1acc029cf9701901d
Joe91 <1015.5@gmx.de>
2022-10-30 20:33:03 +0100

    Revert "Merge pull request #249 from joao-vitor-souza/dev"
    This reverts commit d94afa55180dd5addea25890281f642e864b80dc, reversing
    changes made to 45152266d0f481ae6360716d16df330e963e5583.

--- ext/Shared/ArrayMap.lua
+++ ext/Shared/ArrayMap.lua
@@ -1,59 +1,67 @@
 ---@class ArrayMap
 ---@overload fun():ArrayMap
-ArrayMap = class('ArrayMap')
+ArrayMap = class 'ArrayMap'
 
 function ArrayMap:__init()
-    self._Entries = {}
+	self._entries = {}
 end
 
 function ArrayMap:add(p_Value)
-    table.insert(self._Entries, p_Value)
+	table.insert(self._entries, p_Value)
 end
 
 function ArrayMap:deleteByIndex(p_Index)
-    table.remove(self._Entries, p_Index)
+	table.remove(self._entries, p_Index)
 end
 
 function ArrayMap:exists(p_Value)
-    for l_Key, l_Data in pairs(self._Entries) do
-        if p_Value == l_Data then
-            return true
-        end
-    end
+	local s_Index = {}
 
-    return false
+	for l_Key, l_Data in pairs(self._entries) do
+		s_Index[l_Data] = l_Key
+	end
+
+	if s_Index[p_Value] ~= nil then
+		return true
+	end
+
+	return false
 end
 
 function ArrayMap:delete(p_Value)
-    for l_Key, l_Data in pairs(self._Entries) do
-        if p_Value == l_Data then
-            self:deleteByIndex(l_Key)
-        end
-    end
+	local s_Index = {}
+
+	for l_Key, l_Data in pairs(self._entries) do
+		s_Index[l_Data] = l_Key
+	end
+
+	if s_Index[p_Value] ~= nil then
+		self:deleteByIndex(s_Index[p_Value])
+	end
 end
 
 function ArrayMap:isEmpty()
-    return self:count() == 0
+	return self:count() == 0
 end
 
 function ArrayMap:clear()
-    self._Entries = {}
+	self._entries = {}
 end
 
 function ArrayMap:count()
-    return #self._Entries
+	return #self._entries
 end
 
 function ArrayMap:getEntries()
-    return self._Entries
+	return self._entries
 end
 
 function ArrayMap:join(p_Character)
-    return table.concat(self._Entries, p_Character)
+	return table.concat(self._entries, p_Character)
 end
 
 function ArrayMap:_tostring()
-    return '[ArrayList Count=' .. self:count() .. ', ' .. json.encode(self._Entries) .. ']'
+	return '[ArrayList Count=' .. self:count() .. ', ' .. json.encode(self._entries) .. ']'
 end
 
 return ArrayMap
