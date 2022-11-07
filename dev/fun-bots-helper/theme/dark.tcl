44f2dd89b9c0047a50661930884357e7740b5167
João Vitor <joaovitorsouza205@hotmail.com>
2022-11-07 14:51:17 -0300

    style: dropdown and entry dark theme

--- dev/fun-bots-helper/theme/dark.tcl
+++ dev/fun-bots-helper/theme/dark.tcl
@@ -24,6 +24,7 @@ namespace eval ttk::theme::azure-dark {
             -selectbg       "#007fff"
         }
 
+        # Buttons
         ttk::style layout TButton {
             Button.button -children {
                 Button.padding -children {
@@ -42,6 +43,67 @@ namespace eval ttk::theme::azure-dark {
                 selected $I(rect-basic) \
                 active $I(button-hover) \
             ] -border 4 -sticky ewns
+        
+
+        # Entry
+        ttk::style element create Entry.field \
+            image [list $I(box-basic) \
+                {focus hover} $I(box-accent) \
+                invalid $I(box-invalid) \
+                disabled $I(box-basic) \
+                focus $I(box-accent) \
+                hover $I(box-hover) \
+            ] -border 5 -padding {8} -sticky news
+
+
+        # Combobox
+        ttk::style layout TCombobox {
+            Combobox.field -sticky nswe -children {
+                Combobox.padding -expand true -sticky nswe -children {
+                    Combobox.textarea -sticky nswe
+                }
+            }
+            Combobox.button -side right -sticky ns -children {
+                Combobox.arrow -sticky nsew
+            }
+        }
+
+        ttk::style map TCombobox -selectbackground [list \
+            {!focus} $colors(-selectbg) \
+            {readonly hover} $colors(-selectbg) \
+            {readonly focus} $colors(-selectbg) \
+        ]
+            
+        ttk::style map TCombobox -selectforeground [list \
+            {!focus} $colors(-selectfg) \
+            {readonly hover} $colors(-selectfg) \
+            {readonly focus} $colors(-selectfg) \
+        ]
+
+        ttk::style element create Combobox.field \
+            image [list $I(box-basic) \
+                {readonly disabled} $I(rect-basic) \
+                {readonly pressed} $I(rect-basic) \
+                {readonly focus hover} $I(button-hover) \
+                {readonly focus} $I(button-hover) \
+                {readonly hover} $I(button-hover) \
+                {focus hover} $I(box-accent) \
+                readonly $I(rect-basic) \
+                invalid $I(box-invalid) \
+                disabled $I(box-basic) \
+                focus $I(box-accent) \
+                hover $I(box-hover) \
+            ] -border 5 -padding {8}
+            
+        ttk::style element create Combobox.button \
+            image [list $I(combo-button-basic) \
+                 {!readonly focus} $I(combo-button-focus) \
+                 {readonly focus} $I(combo-button-hover) \
+                 {readonly hover} $I(combo-button-hover)
+            ] -border 5 -padding {2 6 6 6}
+
+        ttk::style element create Combobox.arrow image $I(down) \
+            -width 15 -sticky e
 
     }
 }
