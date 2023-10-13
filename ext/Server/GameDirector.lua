493b6cee5e1651de683d1f91ff9728046f19938e
Tagener Noisu <op4.renegat@gmail.com>
2023-10-13 15:07:05 +0600

    Do not spawn on vehicle paths

--- ext/Server/GameDirector.lua
+++ ext/Server/GameDirector.lua
@@ -829,7 +829,7 @@ function GameDirector:GetSpawnPath(p_TeamId, p_SquadId, p_OnlyBase)
 
 			local s_Node = m_NodeCollection:Get(1, l_Path)
 
-			if s_Node == nil or s_Node.Data.Objectives == nil or #s_Node.Data.Objectives ~= 1 then
+			if s_Node == nil or s_Node.Data.Objectives == nil or #s_Node.Data.Objectives ~= 1 or s_Node.Data.Vehicles ~= nil then
 				goto continue_paths_loop
 			end
 
