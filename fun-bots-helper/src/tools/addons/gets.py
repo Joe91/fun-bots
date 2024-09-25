083f1a5cf58443f44892fde21dfed14ed28b43a1
Joe <1015.5@gmx.de>
2024-09-25 21:47:34 +0200

    fix in mapfile-generation

--- fun-bots-helper/src/tools/addons/gets.py
+++ fun-bots-helper/src/tools/addons/gets.py
@@ -393,8 +393,9 @@ def get_map_lines_created() -> List[List]:
         - map_items - A list of all maps' information
     """
     rounds_to_use = "1"
-    maps_with_gunmaster = ["XP2", "XP4"]
-    maps_without_tdm_cq = ["XP2"]
+    maps_with_gunmaster = ["XP2", "XP4", "SP", "COOP"]
+    maps_without_tdm_large = ["XP2"]
+    mapnames_without_tdm_large = ["COOP_002", "SP_Villa"]
 
     map_items = []
 
@@ -412,11 +413,11 @@ def get_map_lines_created() -> List[List]:
                 break
 
         if game_mode in ALL_GAME_MODES:
-            if game_mode == "TDM":
+            if game_mode == "TDM": # in tdm, other modes are directly possible as well...
                 if mapname_splitted in maps_with_gunmaster:
                     map_items.append([mapname, "GunMaster0", rounds_to_use])
-                if mapname_splitted not in maps_without_tdm_cq:
-                    map_items.append([mapname, translated_game_mode, rounds_to_use])
+                if (mapname_splitted not in maps_without_tdm_large) and (mapname not in mapnames_without_tdm_large):
+                    map_items.append([mapname, "TeamDeathMatch0", rounds_to_use])
                 map_items.append([mapname, "TeamDeathMatchC0", rounds_to_use])
             else:
                 map_items.append([mapname, translated_game_mode, rounds_to_use])
